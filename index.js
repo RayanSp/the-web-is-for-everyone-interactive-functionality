ca143d4314ff672e5f2a6cdb884fc23c271e8066
RayanSp <rayan.spall@hva.nl>
2023-03-27 12:24:56 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express'
 
-const url = 'https://api.vinimini.fdnd.nl/api/v1'
-
 // Maak een nieuwe express app
 const app = express()
 
@@ -18,9 +16,10 @@ app.use(express.urlencoded({ extended: true }))
 
 // Maak een route voor de index
 app.get('/', (request, response) => {
-  let semesterUrl = url + '/producten'
+  const url = 'https://api.vinimini.fdnd.nl/api/v1'
+  const noteUrl = url + '/notities'
 
-  fetchJson(url).then((data) => {
+  fetchJson(noteUrl).then((data) => {
     console.log(data)
     response.render('index', data)
   })
@@ -38,8 +37,43 @@ app.get('/contact', (request, response) => {
   response.render('contact')
 })
 
-app.get('/new', (request, response) => {
-    response.render('form')
+// app.get('/notities', (request, response) => {
+//     response.render('form')
+//   })
+
+  app.post('/notities', (request, response) => {
+    const url = 'https://api.vinimini.fdnd.nl/api/v1'
+    const noteUrl = url + '/notities'
+
+    request.body.afgerond = false
+    request.body.persoonId = 'clemozv3c3eod0bunahh71sx7'
+    request.body.datum = request.body.datum + ':00Z'
+    request.body.herinnering = [request.body.herinnering + ':00Z']
+    // request.body.aanmelddatum = (new Date()).toISOString();
+    
+    postJson(url + '/notities', request.body).then((data) => {
+      let newNote = { ... request.body }
+      console.log(newNote);
+
+
+
+      if (data.success) {
+        response.redirect('/') 
+        console.log("werkt!")
+        console.log(data);
+        
+        // TODO: squad meegeven, message meegeven
+        // TODO: Toast meegeven aan de homepagina
+      } 
+      
+      else {
+        const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
+        const newdata = { error: errormessage, values: newNote }
+        console.log(data)
+        console.log(JSON.stringify(data))
+        response.render('index', newdata)
+      }
+    })
   })
 
 // Stel het poortnummer in en start express
@@ -54,10 +88,10 @@ app.listen(app.get('port'), function () {
  * @returns the json response from the api endpoint
  */
 async function fetchJson(url) {
-    return await fetch(url)
-      .then((response) => response.json())
-      .catch((error) => error)
-  }
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
 
 export async function postJson(url, body) {
     return await fetch(url, {
